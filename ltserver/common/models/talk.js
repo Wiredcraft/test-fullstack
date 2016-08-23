cd69f120deebdc312e914fe6ef335e8c76fc7344
haishanh <haishanhan@gmail.com>
2016-08-23 20:10:52 +0800

    Refine model relation

--- ltserver/common/models/talk.js
+++ ltserver/common/models/talk.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Talk) {
+  Talk.greet = function(cb) {
+    cb(null, 'Greetings...');
+  }
 
+  Talk.remoteMethod(
+    'greet',{
+      http: {
+        path: '/greet',
+        verb: 'get'
+      },
+      returns: {
+        arg: 'greeting',
+        type: 'string'
+      }
+    }
+  );
+
+  Talk.afterRemote('find', function(ctx, ins, next) {
+    console.log(ctx.req.accessToken);
+    console.log(ctx.result);
+    next();
+  });
 };
--- ltserver/server/server.js
+++ ltserver/server/server.js
@@ -4,6 +4,7 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
+app.set('json spaces', 2); // TODO to remove
 
 app.start = function() {
   // start the web server
