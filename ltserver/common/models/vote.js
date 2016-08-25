d3542dc1bf58c0a00e6badbf524262bf968e598e
haishanh <haishanhan@gmail.com>
2016-08-25 15:36:23 +0800

    Add new remote method for Vote model

--- ltserver/common/models/vote.js
+++ ltserver/common/models/vote.js
@@ -3,42 +3,53 @@
 var debug = require('debug')('lt');
 var app = require('../../server/server');
 
-function updateTalkVoteCount(ctx, num) {
-  if (ctx.instance) {
-    var Talk = app.models.Talk;
-    Talk.findById(ctx.instance.talkId, function (err, talk) {
-      // ignore err
-      debug('update voteCount');
-      debug('before: ' + talk.voteCount);
-      // console.log(talk);
-      talk.updateAttribute('voteCount', talk.voteCount + num, function (err, talk) {
-        if (err) throw err;
-        debug('after: ' + talk.voteCount);
-      });
+function updateTalkVoteCount(talkId, num, cb) {
+  var Talk = app.models.Talk;
+  Talk.findById(talkId, function (err, talk) {
+    // ignore err
+    debug('update voteCount');
+    debug('before: ' + talk.voteCount);
+    // console.log(talk);
+    talk.updateAttribute('voteCount', talk.voteCount + num, function (err, talk) {
+      if (err) cb(err);
+      debug('after: ' + talk.voteCount);
+      cb();
     });
-  } // no else
+  });
 }
 
 
 module.exports = function(Vote) {
-
+  /*
+   * new remote method `upvote`
+   * endpoint /api/Votes/upvote - POST
+   * the post data will be used as the filter to query the db to find
+   * a Vote instance
+   * if it can find one, the instance will be deleted, relevant talks
+   * voteCount will be decreased by 1
+   * if it can not find one, a new instance will be created, relevant
+   * talks voteCount will be increased by 1 
+   */
   Vote.upvote = function(talkId, voterId, cb) {
-    debug('args are ' + talkId + ' ' + voterId);
-    var data = {talkId: talkId, voterId: voterId};
+    var data = {talkId: talkId, voterId: voterId}; // used as filter
     Vote.findOrCreate({ where: data}, data, function(err, ins, created) {
       if (err) cb(err);
       if (created) {
         // create
-        debug('created vote: ')
-        debug(ins);
-        cb(null, ins.talkId, ins.voterId, ins.id);
+        // new vote, increase corresponding talks voteCount
+        updateTalkVoteCount(talkId, 1, function(err) {
+          if(err) cb(err);
+          cb(null, ins.talkId, ins.voterId, ins.id);
+        });
       } else {
         // exist
-        Vote.destroyAll({ where: data}, function(err, info) {
+        // if exist, we delete it and decrease corresponding talks voteCount
+        Vote.destroyById(ins.id, function(err) {
           if(err) cb(err);
-          debug('destroy vote: ')
-          debug(ins)
-          cb(null, ins.talkId, ins.voterId, ins.id);
+          updateTalkVoteCount(talkId, -1, function(err) {
+            if (err) cb(err);
+            cb(null, ins.talkId, ins.voterId, ins.id);
+          });
         });
       }
     });
@@ -55,15 +66,5 @@ module.exports = function(Vote) {
       {arg: 'id', type: 'number'}
     ],
     http: {path: '/upvote', verb: 'post'}
-  });
-
-  Vote.observe('before save', function (ctx, next) {
-    updateTalkVoteCount(ctx, 1);
-    next();
-  });
-
-  Vote.observe('before delete', function (ctx, next) {
-    updateTalkVoteCount(ctx, -1);
-    next();
   });
 };
