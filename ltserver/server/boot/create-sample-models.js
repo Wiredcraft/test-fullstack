6c566067531721b1fcd88382226e9b2d6bf413ac
haishanh <haishanhan@gmail.com>
2016-08-30 00:36:15 +0800

    Using more "real" sample data

--- ltserver/server/boot/create-sample-models.js
+++ ltserver/server/boot/create-sample-models.js
@@ -12,35 +12,34 @@ module.exports = function(app) {
   var Vote = app.models.Vote;
 
   var appUsers = [
-    {username: 'alice', email: 'alice@example.com', password: 'quickfox'},
-    {username: 'bob', email: 'bob@example.com', password: 'quickfox'},
-    {username: 'charlie', email: 'charlie@example.com', password: 'quickfox'},
+    {username: 'alice', email: 'alice@example.com', password: 'alice'},
+    {username: 'bob', email: 'bob@example.com', password: 'bob'},
+    {username: 'charlie', email: 'charlie@example.com', password: 'charlie'},
   ];
 
   var DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
   var talks = [
     {
-      title: 'How to beat procratination',
-      description: 'Jon talks about how to beat procratination',
-      speaker: 'Jon Snow',
-      cover: 'http://carlog.qiniudn.com/starry-night.jpg',
-      createdAt: new Date(),
+      title: 'Inside the mind of a master procrastinator',
+      description: "Tim Urban knows that procrastination doesn't make sense, but he's never been able to shake his habit of waiting until the last minute to get things done. In this hilarious and insightful talk, Urban takes us on a journey through YouTube binges, Wikipedia rabbit holes and bouts of staring out the window — and encourages us to think harder about what we're really procrastinating on, before we run out of time.",
+      speaker: 'Tim Urban',
+      cover: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/4d10247c2b89098e6f7103065276923e9432e914_2880x1620.jpg?cb=05112016&quality=89&w=700',
+      createdAt: new Date() - DAY_IN_MILLISECONDS / 6,
       voteCount: 1,
     },
     {
-      title: 'What happens after you click a link in your browser',
-      description: 'John talks about what happens after you click' +
-        ' a link in your browser',
-      speaker: 'Arya Stark',
-      cover: 'http://carlog.qiniudn.com/starry-night.jpg',
+      title: 'Animal tales from icy wonderlands',
+      description: 'Diving under the Antarctic ice to get close to the much-feared leopard seal, photographer Paul Nicklen found an extraordinary new friend. Share his hilarious, passionate stories of the polar wonderlands, illustrated by glorious images of the animals who live on and under the ice.',
+      speaker: 'Paul Nicklen',
+      cover: 'https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/1f6a9f396100ba693c5534a3a8ca597244ff40c6_800x600.jpg?cb=05112016&quality=89&w=700',
       createdAt: new Date() - DAY_IN_MILLISECONDS,
       voteCount: 2,
     },
     {
-      title: '10 thinks you did not know about React',
-      description: 'Bran talks about how to beat procratination',
-      speaker: 'Bran Snow',
-      cover: 'http://carlog.qiniudn.com/starry-night.jpg',
+      title: 'Is our universe the only universe?',
+      description: 'Is there more than one universe? In this visually rich, action-packed talk, Brian Greene shows how the unanswered questions of physics (starting with a big one: What caused the Big Bang?) have led to the theory that our own universe is just one of many in the "multiverse."',
+      speaker: 'Brian Greene',
+      cover: 'http://i.imgur.com/i937FSq.jpg',
       createdAt: new Date() - DAY_IN_MILLISECONDS * 2,
       voteCount: 1,
     },
