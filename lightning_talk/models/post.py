02a859ba5f11c636f934eb1b1c40ab045106e539
Yiwan Zhou <zhouyiwan@gmail.com>
2017-05-22 13:50:41 +0800

    change post model field

--- lightning_talk/models/post.py
+++ lightning_talk/models/post.py
@@ -5,9 +5,8 @@ from django.conf import settings
 class Post(models.Model):
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=200)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    rating = models.PositiveIntegerField(default=0)
 
-    @property
-    def username(self):
-        return self.user.username
-
+    class Meta:
+        ordering = ['-rating']
