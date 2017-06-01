36b9da471c3ed0fbf319267c2012c48c342837cd
Yiwan Zhou <zhouyiwan@gmail.com>
2017-06-01 21:15:08 +0800

    user model HyperlinkedModelSerializer => ModelSerializer

--- lightning_talk/views/user.py
+++ lightning_talk/views/user.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ('username',)
