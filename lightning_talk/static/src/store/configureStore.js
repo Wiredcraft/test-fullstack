ce6e4fe224ac6a81cba82b12f38c8b8f754cec17
Yiwan Zhou <zhouyiwan@gmail.com>
2017-06-02 05:24:51 +0800

    persist user reducer with localstorage

--- lightning_talk/static/src/store/configureStore.js
+++ lightning_talk/static/src/store/configureStore.js
@@ -1,19 +1,26 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 
 import reducers from 'reducers'
 
-export default function configureStore () {
+export default function configureStore() {
   let store = createStore(reducers, applyMiddleware(thunk))
 
   if (process.env.NODE_ENV === 'development') {
-    store = createStore(reducers, applyMiddleware(thunk, createLogger({ collapsed: true })))
+    store = createStore(
+      reducers,
+      undefined,
+      compose(applyMiddleware(thunk, createLogger({ collapsed: true })), autoRehydrate())
+    )
   }
 
   if (module.hot) {
     module.hot.accept('reducers', () => store.replaceReducer(require('reducers').default))
   }
+
+  persistStore(store, { whitelist: ['user'] })
 
   return store
 }
