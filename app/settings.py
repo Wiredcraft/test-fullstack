505bf6631ae25c3bdb08ae15a101c92bb772e46f
Yiwan Zhou <zhouyiwan@gmail.com>
2017-06-03 21:05:25 +0800

    django setting

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS':
-    'rest_framework.pagination.LimitOffsetPagination',
+    'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':
     10
 }
