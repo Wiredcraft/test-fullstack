7f3e4452c3a4f1c2a2712e323cdc940229d4b2a8
shuaibird <shuaibird@icloud.com>
2018-01-02 21:32:53 +0800

    style: add media query rule for mobile; tiny tweaks

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -3,22 +3,12 @@ import PropTypes from 'prop-types'
 
 const Title = ({ title = 'Lightning Talks', onOpenModal} = {}) => (
     <div
-        style={{
-            fontFamily: 'Gabriela, sans-serif',
-            textAlign: 'center',
-            color: 'white',
-            fontSize: '3.5em',
-            marginBottom: '1em',
-        }}
+        className='app-title'
     >
         {title}
         <i
             className='fa fa-plus'
             aria-hidden='true'
-            style={{
-                cursor: 'pointer',
-                marginLeft: '1em',
-            }}
             onClick={onOpenModal}
         />
     </div>
