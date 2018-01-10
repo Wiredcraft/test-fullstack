07acfdcd6e0eabf94faf962fe0b18888e8b11ed0
jiangzifan <j22melody@gmail.com>
2018-01-10 23:41:18 +0800

    basic form validation

---
+++ src/components/Input.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+import './Input.css'
+
+class Input extends Component {
+  render() {
+    const {className, tag, violation, ...rest} = this.props
+
+    const props = {
+      ...rest,
+      className: 'Input',
+    }
+
+    return (
+      <div className={cx('Input-wrapper', className)}>
+        {React.createElement(tag, props)}
+        {violation &&
+          <div className="Input-violation">
+            {violation}
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+Input.propTypes = {
+  tag: PropTypes.oneOf(['input', 'textarea']),
+  value: PropTypes.string,
+  violation: PropTypes.string,
+}
+
+Input.defaultProps = {
+  tag: 'input',
+}
+
+export default Input
--- src/components/TalkBox.js
+++ src/components/TalkBox.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import Input from './Input'
 import './TalkBox.css'
+
+const violationTypes = {
+  REQUIRED_EMPTY: {
+    value: 'REQUIRED_EMPTY',
+    text: () => 'Please fill out the field.',
+  },
+  LENGTH_TOO_LONG: {
+    value: 'LENGTH_TOO_LONG',
+    text: (data) => `Exceed words limit by ${data.exceedBy}`,
+  },
+}
+
+const validationRules = {
+  author: {
+    required: true,
+    maxLength: 19,
+  },
+  title: {
+    required: true,
+    maxLength: 59,
+  },
+  description: {
+    required: true,
+    maxLength: 599,
+  },
+}
 
 const emptyState = {
   author: '',
+  authorViolation: null,
   title: '',
+  titleViolation: null,
   description: '',
+  descriptionViolation: null,
 }
 
 class TalkBox extends Component {
@@ -13,34 +43,81 @@ class TalkBox extends Component {
 
   onChangeAuthor = (event) => {
     this.setState({
-      author: event.target.value
+      author: event.target.value,
+    }, () => {
+      this.validate('author')
     })
   }
 
   onChangeTitle = (event) => {
     this.setState({
-      title: event.target.value
+      title: event.target.value,
+    }, () => {
+      this.validate('title')
     })
   }
 
   onChangeDescription = (event) => {
     this.setState({
-      description: event.target.value
+      description: event.target.value,
+    }, () => {
+      this.validate('description')
     })
+  }
+
+  validate = (fieldName) => {
+    const {required, maxLength} = validationRules[fieldName]
+    const value = this.state[fieldName]
+    const violationKey = `${fieldName}Violation`
+    let violation = null
+
+    if (required && !value) {
+      violation = {
+        type: violationTypes.REQUIRED_EMPTY.value,
+      }
+    } else if (maxLength < value.length) {
+      violation = {
+        type: violationTypes.LENGTH_TOO_LONG.value,
+        data: {
+          exceedBy: value.length - maxLength,
+        },
+      }
+    }
+
+    this.setState({
+      [violationKey]: violation,
+    })
+
+    return !violation
+  }
+
+  validateAll = () => {
+    return Object.keys(validationRules).reduce((result, field) => {
+      return this.validate(field) && result
+    }, true)
   }
 
   onSubmit = (event) => {
     event.preventDefault()
 
-    this.props.onAddTalk({
-      ...this.state
-    })
+    if (this.validateAll()) {
+      this.props.onAddTalk({
+        ...this.state
+      })
 
-    this.setState(emptyState)
+      this.setState(emptyState)
+    }
   }
 
   render() {
-    const {author, title, description} = this.state
+    const {author, authorViolation, title, titleViolation, description, descriptionViolation}
+      = this.state
+    const authorViolationText = authorViolation &&
+      violationTypes[authorViolation.type].text(authorViolation.data)
+    const titleViolationText = titleViolation &&
+      violationTypes[titleViolation.type].text(titleViolation.data)
+    const descriptionViolationText = descriptionViolation &&
+      violationTypes[descriptionViolation.type].text(descriptionViolation.data)
 
     return (
       <form
@@ -55,12 +132,11 @@ class TalkBox extends Component {
           >
             your name:
           </label>
-          <input
-            autoFocus
-            required
+          <Input
             id="TalkBox-author"
             className="TalkBox-input"
             value={author}
+            violation={authorViolationText}
             onChange={this.onChangeAuthor}
           />
         </div>
@@ -72,11 +148,11 @@ class TalkBox extends Component {
           >
             title:
           </label>
-          <input
-            required
+          <Input
             id="TalkBox-title"
             className="TalkBox-input"
             value={title}
+            violation={titleViolationText}
             onChange={this.onChangeTitle}
           />
         </div>
@@ -88,11 +164,13 @@ class TalkBox extends Component {
           >
             text:
           </label>
-          <textarea
+          <Input
+            tag="textarea"
             id="TalkBox-text"
             className="TalkBox-input"
             rows={8}
             value={description}
+            violation={descriptionViolationText}
             onChange={this.onChangeDescription}
           />
         </div>
