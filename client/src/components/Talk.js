dd8503b8da81ae35e8b59e3d86c8d5a2987a2e54
Natalia Vetter <nataliavetter@gmail.com>
2018-01-16 23:56:46 +0100

    Add icon for upvoting and add favicon

--- client/src/components/Talk.js
+++ client/src/components/Talk.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import '../assets/sass/Talk.css';
+import React from 'react'
+import '../assets/sass/Talk.css'
 
 const Talk = (props) =>
   <div className='c-Talk'>
     <div className='c-Talk__rating'>
       <span>{props.talk.rating}</span>
       <button
+        className='c-Talk__btn'
         onClick={props.onUpvoteClick.bind(this, props.talk.id)}
       >
-        Upvote
       </button>
     </div>
     <div>
