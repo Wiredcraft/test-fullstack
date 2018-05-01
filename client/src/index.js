ca3455337d96f0e6810c2bc1f6020a15c907cdc8
mil22n <milan.djordjevic22@gmail.com>
2018-05-01 17:26:43 +0200

    Project and environment setup

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div></div>, document.getElementById('container'));
+registerServiceWorker();
