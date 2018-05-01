bd52ad92cfb80db41d511d085d4932c703eea89c
mil22n <milan.djordjevic22@gmail.com>
2018-05-01 19:17:39 +0200

    Add configuration for development environment

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div></div>, document.getElementById('container'));
+ReactDOM.render(<div>{process.env.REACT_APP_API_URL}</div>, document.getElementById('container'));
 registerServiceWorker();
