56dc2dc9ee60f22ecbc952cdccdd4ca99146b304
mil22n <milan.djordjevic22@gmail.com>
2018-05-03 18:38:25 +0200

    Refactor talks action tests into smaller chunks

--- client/src/actions/talks.actions.test.js
+++ client/src/actions/talks.actions.test.js
@@ -6,43 +6,75 @@ import { FETCH_TALKS, CREATE_TALK, UPDATE_TALK } from './types';
 import { fetchTalks, createTalk, updateTalk } from './talks.actions';
 
 describe('Talk actions', () => {
+
+    let callback, promise;
+    beforeEach(() => {
+        callback = jest.fn();
+        promise = new Promise(resolve => resolve(response));
+    });
+
     describe('fetchTalks', () => {
+        
         it('has the correct type and payload', () => {
             const action = fetchTalks();
 
             expect(action.type).toEqual(FETCH_TALKS);
             expect(action.payload).toEqual(new Promise(resolve => resolve(talks)));
+        });
+
+        it('makes the correct api call', () => {
+            fetchTalks();
+
             expect(api.get).toHaveBeenCalledWith('talks?filter[order]=rating DESC');
         });
     });
 
     describe('createTalk', () => {
+
         it('has the correct type and payload', () => {
-            const callback = jest.fn();
             const action = createTalk({}, callback);
-            const promise = new Promise(resolve => {return resolve(response)});
 
             expect(action.type).toEqual(CREATE_TALK);
             expect(action.payload).toEqual(promise);
+        });
+
+        it('makes the correct api call', () => {
+            createTalk({}, callback);
+
+            expect(api.post).toHaveBeenCalledWith('talks', {});
+        });
+
+        it('makes the callback call on resolved promise', () => {
+            createTalk({}, callback);
 
             promise.then(() => {
                 expect(callback).toHaveBeenCalled();
             });
-        });
+        })
     });
 
     describe('updateTalk', () => {
+
         it('has the correct type and payload', () => {
-            const callback = jest.fn();
             const action = updateTalk({}, callback);
-            const promise = new Promise(resolve => {return resolve(response)});
 
             expect(action.type).toEqual(UPDATE_TALK);
             expect(action.payload).toEqual(promise);
+            expect(api.put).toHaveBeenCalledWith('talks', {});
+        });
+
+        it('makes the correct api call', () => {
+            updateTalk({}, callback);
+
+            expect(api.put).toHaveBeenCalledWith('talks', {});
+        });
+
+        it('makes the callback call on resolved promise', () => {
+            updateTalk({}, callback);
 
             promise.then(() => {
                 expect(callback).toHaveBeenCalled();
             });
-        });
+        })
     });
 });
