4c12ec61b4d5d90edaa5931a135e34373ecfd20e
Bilal Korir <seekingk@live.com>
2018-05-17 17:12:19 +0800

    Implement sing up functionality

---
+++ lightning-talk-polling-app/src/components/SignUp.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {Auth} from 'aws-amplify';
+
+class SignUp extends Component {
+    state = {
+        username: '',
+        password: '',
+        email: '',
+        phone_number: '',
+        authCode: ''
+    }
+
+    onChange = (key, value) => {
+        this.setState({
+            [key]: value
+        });
+    }
+
+    signUp = () => {
+        const {username, password, email, phone_number} = this.state;
+        Auth.signUp({
+            username,
+            password,
+            attributes: {
+                email,
+                phone_number
+            }
+        })
+            .then(() => console.log('Waiting for confirmation to join LIGHT NINNNNNG'))
+            .catch(error => console.log('Error singing up: ', error));
+    }
+
+    confirmSignUp = () => {
+        Auth.confirmSignUp(this.state.username, this.state.authCode)
+            .then(() => console.log('Registration confirmed! welcome to LIGHT NINNNNNG'))
+            .catch(error => console.log('Error confirming registration: ', error));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Sign up</h2>
+            </div>
+        );
+    }
+}
+
+export default SignUp;
