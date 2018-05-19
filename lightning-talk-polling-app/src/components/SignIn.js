eb83b4b9390079211f228fc243820d5da27298e7
Bilal Korir <seekingk@live.com>
2018-05-19 14:22:29 +0800

    Disable multi factor authentication from Cognito

--- lightning-talk-polling-app/src/components/SignIn.js
+++ lightning-talk-polling-app/src/components/SignIn.js
@@ -16,73 +16,45 @@ class SignIn extends Component {
     }
 
     signIn = () => {
-        Auth.signIn(this.state.username, this.state.password)
-            .then(user => this.setState({ user, showConfirmation: true }))
-            .catch(error => console.log('Error sing in: ', error));
-    }
-
-    confirmSignIn = () => {
         const {history} = this.props;
+        Auth.signIn(this.state.username, this.state.password)
+            .then(user => {
+                this.setState({ user, showConfirmation: true });
+                history.push('/');
 
-        Auth.confirmSignIn(this.state.user, this.state.authCode)
-            .then(user => history.push('/'))
-            .catch(error => console.log('Error confirming sing in: ', error));
+            })
+            .catch(error => console.log('Error sing in: ', error));
     }
 
     render() {
         return (
             <div>
-                {
-                    !this.state.showConfirmation && (
-                        <div>
-                            <div className="form-group">
-                                <label>Username</label>
-                                <input
-                                    type="username"
-                                    className="form-control"
-                                    placeholder="Username"
-                                    onChange={event => this.onChange('username', event.target.value)}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label>Password</label>
-                                <input
-                                    type="password"
-                                    className="form-control"
-                                    placeholder="Password"
-                                    onChange={event => this.onChange('password', event.target.value)}
-                                />
-                            </div>
-                            <button
-                                className="btn btn-primary"
-                                onClick={this.signIn}
-                            >
-                                Sign In
-                            </button>
-                        </div>
-                    )
-                }
-                {
-                    this.state.showConfirmation && (
-                        <div>
-                            <div className="form-group">
-                                <label>Authentication Code</label>
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    placeholder="000111"
-                                    onChange={event => this.onChange('authCode', event.target.value)}
-                                />
-                            </div>
-                            <button
-                                className="btn btn-primary"
-                                onClick={this.confirmSignIn.bind(this)}
-                            >
-                                Confirm Sing In
-                            </button>
-                        </div>
-                    )
-                }
+                <div>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input
+                            type="username"
+                            className="form-control"
+                            placeholder="Username"
+                            onChange={event => this.onChange('username', event.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                            onChange={event => this.onChange('password', event.target.value)}
+                        />
+                    </div>
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.signIn}
+                    >
+                        Sign In
+                    </button>
+                </div>
             </div>
         );
     }
