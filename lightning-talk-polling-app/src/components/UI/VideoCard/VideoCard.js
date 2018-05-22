91408c91f7a8d3d5795b9bc3c5265970788e3fa4
Bilal Korir <seekingk@live.com>
2018-05-22 16:11:09 +0800

    Access project config files via npm run eject

--- lightning-talk-polling-app/src/components/UI/VideoCard/VideoCard.js
+++ lightning-talk-polling-app/src/components/UI/VideoCard/VideoCard.js
@@ -1,27 +1,25 @@
 import React from 'react';
+import cssClass from './VideoCard.css';
 
 const videoCard = (props) => (
-    <div id='videoCard' className='col-md-4'>
-        {/*<div className='col-12 m-1'>*/}
-            <div
-                className="card p-0">
-                <div className="view overlay">
-                    <div className="embed-responsive embed-responsive-16by9">
-                        <iframe className="embed-responsive-item" src={props.url} allowFullScreen/>
-                    </div>
-                </div>
-                <div className="card-body text-left">
-                    <h5 className="card-title">{props.title}</h5>
-                    <hr/>
-                    <p className="card-text">{props.description}</p>
-                </div>
-                <div className="rounded-bottom mdb-color lighten-3 py-3 d-flex justify-content-between">
-                    <i className="fa fa-clock-o pl-1">{props.publishDate}</i>
-                    <i className="fa fa-plus white-text"> {props.points}</i>
-                    <i className="fa fa-user pr-1"> {props.username}</i>
+    <div className='col-md-4'>
+        <div className="card p-0">
+            <div className="view overlay">
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src={props.url} allowFullScreen/>
                 </div>
             </div>
-        {/*</div>*/}
+            <div className="card-body text-left">
+                <h5 className="card-title">{props.title}</h5>
+                <hr/>
+                <p className="card-text">{props.description}</p>
+            </div>
+            <div className="rounded-bottom mdb-color lighten-3 py-3 d-flex justify-content-between">
+                <i className="fa fa-clock-o pl-1">{props.publishDate}</i>
+                <i className="fa fa-plus white-text"> {props.points}</i>
+                <i className="fa fa-user pr-1"> {props.username}</i>
+            </div>
+        </div>
     </div>
 
 );
