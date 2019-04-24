ab8862dbc75749a4331369069c3054b334f238e0
Lionel Rowe <lionel.rowe@gmail.com>
2019-04-24 21:33:18 +0800

    Add proper loading/error states

--- client/src/pages/NewTalk.js
+++ client/src/pages/NewTalk.js
@@ -6,43 +6,63 @@ import config from '../utils/config';
 import UserContext from '../components/UserContext';
 import Title from '../components/Title';
 
+import { fireModal, NETWORK_ERROR } from '../utils/fireModal';
+import  { LOADING, LOADED, ERROR } from '../utils/loadingStatuses';
+
 const NewTalk = () => {
 
   const [ title, setTitle ] = useState('');
   const [ abstract, setAbstract ] = useState('');
+  const [ status, setStatus ] = useState(LOADED);
 
   const { user } = useContext(UserContext);
 
   const submit = async e => {
     e.preventDefault();
 
-    await fetch(`${config.apiHost}/talks`, {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + user,
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title,
-        abstract
-      })
-    });
+    try {
+      setStatus(LOADING);
 
-    navigate(`${process.env.PUBLIC_URL}/`);
+      const res = await fetch(`${config.apiHost}/talks`, {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + user,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          abstract
+        })
+      });
+
+      if (res.status === 200) {
+        navigate(`${process.env.PUBLIC_URL}/`);
+      } else {
+        fireModal(NETWORK_ERROR);
+        setStatus(ERROR);
+      }
+
+    } catch(e) {
+      fireModal(NETWORK_ERROR);
+      setStatus(ERROR);
+    }
+
   };
 
   const cancel = () => navigate(`${process.env.PUBLIC_URL}/`);
 
   return (
-    <form action='#' onSubmit={submit} className='body-container'>
+    <form action='#' onSubmit={submit} className={`form body-container${status === LOADING ? ' loading' : ''}`}>
       <Title text='New Talk' />
       <h2>New Talk</h2>
-      <MaxLengthText getSet={[ title, setTitle ]} label='Title' maxLength={50} />
-      <MaxLengthText getSet={[ abstract, setAbstract ]} label='Abstract' maxLength={250} multiline />
+      <fieldset disabled={status === LOADING}>
+        <MaxLengthText getSet={[ title, setTitle ]} label='Title' maxLength={50} />
+        <MaxLengthText getSet={[ abstract, setAbstract ]} label='Abstract' maxLength={250} multiline />
+      </fieldset>
       <div className='button-group'>
-        <button className='submit-btn' type='submit'>Create</button>
-        <button onClick={cancel} className='cancel-btn' type='button'>Cancel</button>
+        <button disabled={status === LOADING} className='submit-btn' type='submit'>Create</button>
+        <button disabled={status === LOADING} onClick={cancel} className='cancel-btn' type='button'>Cancel</button>
       </div>
     </form>
   );
