24bead9d9b6a3d6b9985ffed16af5e59c021cd45
CarlW <carl@oskarroger.net>
2019-07-20 21:59:02 +0800

    installed apollo-boost react-apollo graphql

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="orange">
-        <h1>Hello World!</h1>
+        <h1>Hacker News Clone</h1>
       </div>
     );
   }
