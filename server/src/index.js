2619d3fcdfe3cb8dde82633bfe19d70236b6739d
CarlW <carl@oskarroger.net>
2019-07-20 22:59:03 +0800

    setting up prisma database

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-    info: String!
-}
-`;
+//The links variable is used to store the links at runtime.
+//For now, everything is stored only in-memory.
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.google.com',
+    description: 'Very common search engine'
+  }
+];
 
+let idCount = links.length;
 const resolvers = {
   Query: {
-    info: () => null
+    info: () => `This is the API of a Hackernews Clone`,
+    //Adding new reslover for the feed root field.
+    //matching the schema definition.
+    feed: () => links
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
