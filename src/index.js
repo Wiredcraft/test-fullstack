be6eb7b59eb22168978cb263e336067444518d56
CarlW <carl@oskarroger.net>
2019-07-20 22:22:26 +0800

    accidentally used yarn which broke the app, had to revert and re-install

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import ReactDOM from 'react-dom';
 import './styles/index.less';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 module.hot.accept();
