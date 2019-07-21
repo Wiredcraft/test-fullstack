1bdc85d39ef43673b0b6f771915ecb9e8ab0b0fb
CarlW <carl@oskarroger.net>
2019-07-21 19:41:58 +0800

    working on the create link component

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 
 export default class CreateLink extends Component {
+    state = {
+        description: "",
+        url: ""
+    }
+
+
   render() {
-    return <div></div>;
+      
+    const { description, url } = this.state;
+
+    return (
+    <div>
+        <div className="flex flex-column mt3">
+            <input 
+            placeholder="A description for the talk"
+            className="mb2"
+            type="text"
+            value={description}
+            onChange={}
+            />
+            <input 
+            placeholder="The URL for the Talk"
+            className="mb2"
+            type="text"
+            value={url}
+            onChange={}
+            />
+        </div>
+        <button onClick={} >Submit</button>
+
+    </div>);
   }
 }
