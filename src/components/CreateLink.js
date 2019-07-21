eb77f61f76dedf4586f571ac47bee10f252e64e9
CarlW <carl@oskarroger.net>
2019-07-21 19:54:21 +0800

    creating post_mutation for the submit button

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
 
-export default class CreateLink extends Component {
-    state = {
-        description: "",
-        url: ""
-    }
+class CreateLink extends Component {
+  state = {
+    description: '',
+    url: ''
+  };
 
+  //Add the input values to the state.
+  handleChange = e => {
+    e.preventDefault();
+    let { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  //TODO: Implement the submit button
+  handleSubmit = e => {
+    console.log('Submitted');
+  };
 
   render() {
-      
     const { description, url } = this.state;
 
     return (
-    <div>
+      <div>
         <div className="flex flex-column mt3">
-            <input 
+          <input
             placeholder="A description for the talk"
             className="mb2"
             type="text"
             value={description}
-            onChange={}
-            />
-            <input 
+            onChange={this.handleChange}
+          />
+          <input
             placeholder="The URL for the Talk"
             className="mb2"
             type="text"
             value={url}
-            onChange={}
-            />
+            onChange={this.handleChange}
+          />
         </div>
-        <button onClick={} >Submit</button>
-
-    </div>);
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
+    );
   }
 }
+
+export default CreateLink;
