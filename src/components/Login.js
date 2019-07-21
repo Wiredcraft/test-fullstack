0ea3ef48dbeac49ae7200f1386f1c295ccfc34a7
CarlW <carl@oskarroger.net>
2019-07-21 20:34:38 +0800

    starting work on the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { AUTH_TOKEN } from '../constants';
 
 export default class Login extends Component {
+  state = {
+    login: true, //Login or Signup
+    email: '',
+    password: '',
+    name: ''
+  };
+
   render() {
     return <div></div>;
   }
