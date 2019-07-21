9704f3795ffee006469a2567af301f7544dec0a1
CarlW <carl@oskarroger.net>
2019-07-21 21:04:24 +0800

    more work on the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,45 @@ export default class Login extends Component {
   };
 
   render() {
-    return <div></div>;
+    const { login, email, password, name } = this.state;
+
+    return (
+      <div>
+        <h4 className="mv3">{login ? 'Login' : 'Sign Up'}</h4>
+        <div className="flex flex-column">
+          {!login && (
+            <input
+              value={name}
+              onChange={e => this.setState({ name: e.target.value })}
+              type="text"
+              placeholder="Name"
+            />
+          )}
+          <input
+            value={email}
+            onChange={e => this.setState({ email: e.target.value })}
+            type="text"
+            placeholder="Email Address"
+          />
+          <input
+            value={password}
+            onChange={e => this.setState({ password: e.target.value })}
+            type="password"
+            placeholder="Super safe password"
+          />
+        </div>
+        <div className="flex mt3">
+          <div className="pointer mr2 button" onClick={() => this._confirm()}>
+            {login ? 'login' : 'create account'}
+          </div>
+          <div
+            className="pointer button"
+            onClick={() => this.setState({ login: !login })}
+          >
+            {login ? 'create account?' : 'got an account?'}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
