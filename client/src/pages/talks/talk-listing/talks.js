8dcdbe8e383d3f5e11f150b43c468cd915d3dd5c
Kun Ran <rankun203@gmail.com>
2019-11-21 03:37:36 +0800

    chore: minor tweaks

--- client/src/pages/talks/talk-listing/talks.js
+++ client/src/pages/talks/talk-listing/talks.js
@@ -9,7 +9,7 @@ export const talks = [
   },
   {
     id: '37a4bd24-55aa-4e12-a049-15cb99ccb279',
-    title: 'The role of the Community and Campus Champions by Gillian Sinclair',
+    title: 'The role of the Community and Campus Champions',
     desc:
       'Help the Software Sustainability Institute with our unknowns by Mike Jackson. How to effectively use the potential of the scientific software user community in producing documentation by Aleksandra Pawlik. Tools for Kinect, research tools and motion analysis by Simon Choppin',
     ctime: 1574276016324,
@@ -18,7 +18,7 @@ export const talks = [
   {
     id: '37a4bd24-55aa-4e12-a049-15cb99ccb289',
     title:
-      'Using Agile Methods in Collaboration with Domain Scientists by Robert Haines',
+      'Using Agile Methods in Collaboration with Domain',
     desc:
       'The Genome Standards Consortium by Norman Morrison. Developing and localising for the international market by Malte Ressin. "Click here" to design your UAV by Dirk Gorissen.',
     ctime: 1574276016324,
@@ -27,7 +27,7 @@ export const talks = [
   {
     id: '37a4bd24-55aa-4e12-a049-15cb99ccb299',
     title:
-      'Help the Software Sustainability Institute with our unknowns by Mike Jackson',
+      'Help the Software Sustainability Institute',
     desc:
       'Software as an Infrastructure: EPSRC Action Plan by Dan Emmerson. The Journal of Open Research Software by Brian Hole.',
     ctime: 1574276016324,
@@ -36,7 +36,7 @@ export const talks = [
   {
     id: '47a4bd24-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Tools for Kinect, research tools and motion analysis by Simon Choppin',
+      'Tools for Kinect, research tools and motion analysis',
     desc:
       'A computational-domain-model-centric process for comparative social simulation by Stuart Rossiter. The YouShare VE: for Developers by Aaron Turner. The importance of ethnography and epistemology on system (hardware and software) design by Sarbjit Bakshi.',
     ctime: 1574276016324,
@@ -44,7 +44,7 @@ export const talks = [
   },
   {
     id: '57a4bd24-55aa-4e12-a049-15cb99ccb269',
-    title: 'The Genome Standards Consortium by Norman Morrison',
+    title: 'The Genome Standards Consortium',
     desc:
       'Statistical modeling of molecular sequence data by Elisa Loza. D3.js for data visualisation by Anna Powell-Smith',
     ctime: 1574276016324,
@@ -53,7 +53,7 @@ export const talks = [
   {
     id: '67a4bd24-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -62,7 +62,7 @@ export const talks = [
   {
     id: '67a4bd25-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -71,7 +71,7 @@ export const talks = [
   {
     id: '67a4bd26-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -80,7 +80,7 @@ export const talks = [
   {
     id: '67a4bd27-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -89,7 +89,7 @@ export const talks = [
   {
     id: '67a4bd28-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -98,7 +98,7 @@ export const talks = [
   {
     id: '67a4bd29-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -107,7 +107,7 @@ export const talks = [
   {
     id: '67a4bd20-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -116,7 +116,7 @@ export const talks = [
   {
     id: '67a4bd21-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -125,7 +125,7 @@ export const talks = [
   {
     id: '67a4bd22-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
@@ -134,7 +134,7 @@ export const talks = [
   {
     id: '67a4bd23-55aa-4e12-a049-15cb99ccb269',
     title:
-      'Developing and localising for the international market by Malte Ressin',
+      'Developing and localising for the international market',
     desc:
       'Data processing at the RCaH Lasers for Science Facility by Steve Fisher. Ondex: an open source data integration, visualisation and analysis framework by Jan Taubert.',
     ctime: 1574276016324,
