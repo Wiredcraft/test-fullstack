5534c7fc0075bd181e0bf51f8aa01505e875381c
Tom Hill <tom.hill991@gmail.com>
2019-11-28 22:01:22 +0800

    fetched talks from an API. Displaying the first index of the API result currently

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ import Form from './components/form.js'
 // initialized lightningTalk object and returned lightningTalk component. It is displayed as App is exported to index where an HTML ID is selected. LightningTalk (the class) passes the properties (props) of lightningTalk from the child component
 
 function App() {
-  const lightningTalk = {
-    title: 'Some title',
-    description: 'Some description'
-  }
+
   return (
     <div>
       < Form />
-      < LightningTalk lightningTalk={lightningTalk} />
+      < LightningTalk />
     </div>
   )
 }
--- src/components/lightning-talk-component.js
+++ src/components/lightning-talk-component.js
@@ -1,12 +1,32 @@
 import React from "react"
 import Buttons from './buttons.js';
 
-// votes initial state is set to 0 as all lightning talks will start with zero votes
+// votes initial state is set to 0 as all lightning talks will start with zero votes. Title and description also set to an empty string until API sets state
 class LightningTalk extends React.Component {
     constructor(props){
     super(props);
-    this.state = {votes: 0}
+    this.state = {
+      title: '',
+      description: '',
+      votes: 0
+    }
   }
+
+componentDidMount = () => {
+  fetch("http://localhost:3000/talks.json")
+  .then(response => response.json())
+  .then((data) => {
+    data.forEach((talk) => {
+      this.setState((state) => {
+        return {
+          title: talk.title,
+          description: talk.description,
+          votes: talk.votes
+        };
+      });
+    });
+  });
+}
 
 // increment and decrement are called when onClick happens on either the Up Vote or Down Vote buttons (in child class Buttons)
    increment = (e) => {
@@ -23,13 +43,11 @@ class LightningTalk extends React.Component {
 
   // when the page renders (loads), pass title and description to parent function (App) as props
   render () {
-    const title = this.props.lightningTalk.title
-    const description = this.props.lightningTalk.description
     return (
         <div className="lightning-talk-item">
           <div className="lt-text">
-            <div className="lt-title"> {title}</div>
-            <div className="lt-description"> {description}</div>
+            <div className="lt-title"> {this.state.title}</div>
+            <div className="lt-description"> {this.state.description}</div>
           </div>
         {/*increment= and decrement= (defined as a prop of Buttons in Buttons class) equals the increment and decrement function of this (LightningTalk) component. This means the state of votes changes onClick of up-vote/down-vote buttons*/}
           <Buttons increment={this.increment} decrement={this.decrement} votes={this.state.votes}/>
