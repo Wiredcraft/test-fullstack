840e63ace4047f8493871e093a8785a003e95bfb
Tom Hill <tom.hill991@gmail.com>
2019-11-29 09:43:24 +0800

    setup API so that all seeds are displayed (moved fetch from lightning-talk-component to APP)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import './App.css';
-import LightningTalk from './components/lightning-talk-component.js'
-import Form from './components/form.js'
+import LightningTalk from './components/lightning-talk-component.js';
+import Form from './components/form.js';
 
-// initialized lightningTalk object and returned lightningTalk component. It is displayed as App is exported to index where an HTML ID is selected. LightningTalk (the class) passes the properties (props) of lightningTalk from the child component
+// initialized state of App to hold an empty lightningTalks compoennt. componentDidMount sets its state depends
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lightningTalks: []
+    };
+  }
 
-function App() {
-
-  return (
-    <div>
-      < Form />
-      < LightningTalk />
-    </div>
-  )
+// componentDidMount is called and sets the state of the lightningTalks array in constructor(props)
+componentDidMount = () => {
+  fetch("http://localhost:3000/talks.json")
+  .then(response => response.json())
+  .then((data) => {
+      this.setState((state) => {
+        return {
+          lightningTalks: data
+        };
+      });
+    });
+  }
+  // now the state of lightning talks depends on what is on the API. Below there is a loop(.map) which is set by componentDidMount
+  render() {
+    return (
+      <div>
+        <Form />
+          <div className="talks">
+            {this.state.lightningTalks.map((talk) => {
+              return <LightningTalk lightningTalk={talk} />
+            })}
+          </div>
+       </div>
+    )
+  }
 }
 
 export default App;
--- src/components/lightning-talk-component.js
+++ src/components/lightning-talk-component.js
@@ -3,41 +3,23 @@ import Buttons from './buttons.js';
 
 // votes initial state is set to 0 as all lightning talks will start with zero votes. Title and description also set to an empty string until API sets state
 class LightningTalk extends React.Component {
-    constructor(props){
+   constructor(props){
     super(props);
     this.state = {
-      title: '',
-      description: '',
       votes: 0
     }
   }
 
-componentDidMount = () => {
-  fetch("http://localhost:3000/talks.json")
-  .then(response => response.json())
-  .then((data) => {
-    data.forEach((talk) => {
-      this.setState((state) => {
-        return {
-          title: talk.title,
-          description: talk.description,
-          votes: talk.votes
-        };
-      });
-    });
-  });
-}
-
-// increment and decrement are called when onClick happens on either the Up Vote or Down Vote buttons (in child class Buttons)
+// Need to change this so that the increment and decrement change the state of votes in App (super?). I assume that because it is a nested array it no longer works
    increment = (e) => {
     this.setState({
-      votes : this.state.votes + 1
+      lightningTalks: this.state.lightningTalk.votes + 1
     })
   }
 
    decrement = (e) => {
     this.setState({
-      votes : this.state.votes - 1
+      lightningTalks: this.state.lightningTalk.votes - 1
     })
   }
 
@@ -46,11 +28,11 @@ componentDidMount = () => {
     return (
         <div className="lightning-talk-item">
           <div className="lt-text">
-            <div className="lt-title"> {this.state.title}</div>
-            <div className="lt-description"> {this.state.description}</div>
+            <div className="lt-title"> {this.props.lightningTalk.title}</div>
+            <div className="lt-description"> {this.props.lightningTalk.description}</div>
           </div>
         {/*increment= and decrement= (defined as a prop of Buttons in Buttons class) equals the increment and decrement function of this (LightningTalk) component. This means the state of votes changes onClick of up-vote/down-vote buttons*/}
-          <Buttons increment={this.increment} decrement={this.decrement} votes={this.state.votes}/>
+          <Buttons increment={this.increment} decrement={this.decrement} votes={this.props.lightningTalk.votes}/>
         </div>
       )
     }
