a6bb172b25e769e615935c92ec5b9f2c802482b6
skillboost <skillboost@protonmail.com>
2019-12-14 15:00:14 +0800

    Add: Basic server configuration + connection to local MongoDB server

---
+++ hacker-news/server.js
@@ -0,0 +1,34 @@
+// server.js
+
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    config = require('./config/DB');
+
+mongoose.Promise = global.Promise;
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(cors());
+
+var port = process.env.PORT || 4000;
+
+mongoose
+    .connect(config.DB)
+    .then((result) => {
+        console.log('Database is connected')
+        app.listen(port, function(){
+            console.log('Listening on port ' + port);
+        })
+    })
+    .catch(err => {
+        console.log('Can not connect to the database'+ err)
+    });
