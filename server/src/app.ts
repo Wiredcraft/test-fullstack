0cf552e824e0070566abf2da204d3c352f49b8b6
Nicolas de Lima <teixlima@gmail.com>
2021-08-26 00:08:51 -0300

    feat: adding router manager into application manager

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,21 @@
 import express, { Application } from "express";
+import { IApplicationConfig } from "./interfaces";
+import RouterManager from "./router/router-manager";
 
 class ApplicationManager {
 	private app: Application;
+	private applicationConfig: IApplicationConfig;
 
-	constructor() {
+	constructor(applicationConfig: IApplicationConfig) {
 		this.app = express();
+		this.applicationConfig = applicationConfig;
+		this.setupApplication();
+	}
+
+	private setupApplication() {
+		const { routesConfig } = this.applicationConfig;
+
+		RouterManager.setRoutesInApplication(routesConfig, this.app);
 	}
 
 	public getServer(): Application {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
 import ApplicationManager from "./app";
+import routesConfig from "./router/routes-config";
 
-const appManager = new ApplicationManager();
+const appManager = new ApplicationManager({ routesConfig });
 
 const server = appManager.getServer();
 
 const PORT = process.env.PORT || "3000";
 
 server.listen(PORT, () => console.log(`Server Running on http://localhost:${PORT}`));
-
-server.get('/', (req, res) => res.json('ok'));
