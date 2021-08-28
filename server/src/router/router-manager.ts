bc59370010188a7de6b55b5cfdc53f712f9c8d83
Nicolas de Lima <teixlima@gmail.com>
2021-08-28 17:41:44 -0300

    improvement: transforming router manager in functions file

--- server/src/router/router-manager.ts
+++ server/src/router/router-manager.ts
@@ -2,26 +2,26 @@ import { Application, Router } from "express";
 import { ICustomRoute } from "../interfaces";
 import { logger } from "../utils";
 
-class RouterManager {
-	static setRoutesInApplication(routesConfig: ICustomRoute[], app: Application) {
-		routesConfig.forEach((config) => {
-			const route = this.createRouteFromConfig(config);
-			app.use(route)
+const setRoutesInApplication = (routesConfig: ICustomRoute[], app: Application) => {
+	routesConfig.forEach((config) => {
+		const route = createRouteFromConfig(config);
+		app.use(route)
 
-			const { method, path } = config;
+		const { method, path } = config;
 
-			logger.info(`Endpoint in use: ${method}-${path}`);
-		});
-	}
-
-	private static createRouteFromConfig(routeConfig: ICustomRoute): Router {
-		const router = Router();
-
-		const { handler, method, path, customMiddleware } = routeConfig;
-
-		customMiddleware ? router[method](path, customMiddleware, handler) : router[method](path, handler);
-		return router;
-	}
+		logger.info(`Endpoint in use: ${method}-${path}`);
+	});
 }
 
-export default RouterManager;
+const createRouteFromConfig = (routeConfig: ICustomRoute): Router => {
+	const router = Router();
+
+	const { handler, method, path, customMiddleware } = routeConfig;
+
+	customMiddleware ? router[method](path, customMiddleware, handler) : router[method](path, handler);
+	return router;
+}
+
+export default {
+	setRoutesInApplication
+}
