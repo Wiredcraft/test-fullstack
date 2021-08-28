205e1a95fbb7f0354a20cc932dca96675889370b
Nicolas de Lima <teixlima@gmail.com>
2021-08-28 17:40:38 -0300

    feat: adding talk mongoose schema

---
+++ server/src/models/talk.ts
@@ -0,0 +1,11 @@
+import { model, Schema, Model } from "mongoose";
+import { ITalk } from "../interfaces/talk";
+
+const TalkSchema: Schema = new Schema({
+	title: { type: String, required: true },
+	description: { type: String, required: true },
+	author: { type: String, required: true },
+	votes: { type: [String] },
+})
+
+export const TalkModel: Model<ITalk> = model('Talk', TalkSchema);
