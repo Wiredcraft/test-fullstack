238d7b56df6c7b50364d0c10a43558bdeaeb1a27
Nicolas de Lima <teixlima@gmail.com>
2021-08-27 22:10:03 -0300

    fix: setting default message and values

--- server/src/middlewares/error.ts
+++ server/src/middlewares/error.ts
@@ -3,9 +3,10 @@ import { ICustomError } from "../interfaces";
 import { logger } from "../utils";
 
 export const errorMiddleware = (err: ICustomError, req: Request, res: Response, next: NextFunction) => {
-	const { message = 'Internal Server Error', statusCode = 500 } = err;
+	let { message, statusCode = 500 } = err;
 
 	logger.error({ err }, 'ERROR HANDLER');
+	message = statusCode === 500 ? 'Internal Server Error' : message;
 
 	res.status(statusCode).json({
 		status: "error",
