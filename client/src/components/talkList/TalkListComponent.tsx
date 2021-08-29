26d911baedb1f5747466473de8e10a53cb540917
Nicolas de Lima <teixlima@gmail.com>
2021-08-29 16:53:51 -0300

    feat: adding talks from server logic

--- client/src/components/talkList/TalkListComponent.tsx
+++ client/src/components/talkList/TalkListComponent.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import { useAuth } from "../../contexts/AuthContext";
 
-function TalkListComponent() {
+type Talk = {
+	title: string;
+	description: string;
+	votes: string[];
+	author: string;
+}
+
+type InputProps = {
+	talks: Talk[]
+}
+
+function TalkListComponent(props: InputProps) {
 	const { isAuthenticated } = useAuth()
 	//This will be map actually and prop being passed will control whether the like button will be there or not	
 	return <Talk props={isAuthenticated}></Talk>
