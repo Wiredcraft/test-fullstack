56add734b5924fe37f6801962fa8c3435a02a8cc
Nicolas Vinicius Teixeira de Lima <nicolasl@ciandt.com>
2021-09-02 00:07:44 -0300

    feat: adding redirect to controller generator

--- server/src/utils/controller-generator.ts
+++ server/src/utils/controller-generator.ts
@@ -5,25 +5,30 @@ import { ServerResponse } from "../interfaces";
 type ControllerFunction = (req: Request) => Promise<ServerResponse>;
 
 /**
- * Encapsulate controller, log envent and treats any thrown error
+ * Encapsulate controller, log event and treats any thrown error
  */
 export const withServiceLayer = (functionToExecute: ControllerFunction) => composeGenerator(functionToExecute)
 
 const composeGenerator = (controller: ControllerFunction) => async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		const { body, query, headers, method } = req;
+  try {
+    const { body, query, headers, method } = req;
 
-		logger.info({
-			method,
-			body,
-			query,
-			headers
-		}, 'EVENT INFO');
+    logger.info({
+      method,
+      body,
+      query,
+      headers
+    }, 'EVENT INFO');
 
 
-		const response = await controller(req);
-		res.json(response);
-	} catch (err) {
-		next(err)
-	}
+    const response = await controller(req);
+    const { redirect } = response;
+    
+    redirect ?
+      res.redirect(redirect) :
+      res.json(response);
+
+  } catch (err) {
+    next(err)
+  }
 }
