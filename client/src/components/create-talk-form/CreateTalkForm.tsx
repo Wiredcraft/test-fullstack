4dbd02f774fd041ec4cd88486b1a2bc8f5886c7f
Nicolas Vinicius Teixeira de Lima <nicolasl@ciandt.com>
2021-09-02 00:11:00 -0300

    feat: adding errors span to form

--- client/src/components/create-talk-form/CreateTalkForm.tsx
+++ client/src/components/create-talk-form/CreateTalkForm.tsx
@@ -1,31 +1,46 @@
 import * as React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, DeepMap, FieldError } from "react-hook-form";
 import "./createTalkForm.css";
 
 type FormInput = {
-	title: String;
-	description: String;
+  title: String;
+  description: String;
 }
 
 type InputProps = {
-	closeForm: Function;
+  closeForm: Function;
 }
 
+type ErrorForm = (DeepMap<String, FieldError> & Partial<FieldError>) | undefined
+
+
 function CreateTalkForm(props: InputProps) {
-	const { closeForm } = props;
+  const { closeForm } = props;
 
-	const { register, handleSubmit } = useForm<FormInput>();
-	const onSubmit: SubmitHandler<FormInput> = data => { closeForm()};
+  const { register, handleSubmit, formState: { errors } } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = data => { closeForm() };
 
-	return (
-		<form className="create-talk-form" onSubmit={handleSubmit(onSubmit)}>
-			<label>Title</label>
-			<input {...register("title", { required: true, maxLength: 20 })} />
-			<label>Description</label>
-			<textarea {...register("description", { required: true, maxLength: 200 })} />
-			<input className="create-talk-form--submit" type="submit" />
-		</form>
-	);
+  const showErrors = (error: ErrorForm) => {
+    if (error) {
+      return (
+        <div className="create-talk-form--error">
+          <span>This field is required</span>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <form className="create-talk-form" onSubmit={handleSubmit(onSubmit)}>
+      <label>Title</label>
+      <input {...register("title", { required: true, maxLength: 20 })} />
+      {showErrors(errors.title)}
+      <label>Description</label>
+      <textarea {...register("description", { required: true, maxLength: 200 })} />
+      {showErrors(errors.description)}
+      <input className="create-talk-form--submit" type="submit" />
+    </form>
+  );
 }
 
 export default CreateTalkForm;
