b44cdd4bbdbad340bf48ab2ddce01abf9c413c05
Nicolas Vinicius Teixeira de Lima <teixlima@gmail.com>
2021-09-04 22:19:54 -0300

    feat: adding notification logic

--- client/src/services/talks.ts
+++ client/src/services/talks.ts
@@ -1,5 +1,13 @@
 import axios from "axios"
 import { TalkCardInfo } from "../types/talk";
+import Swal from 'sweetalert'
+
+const notify = (message: string, isSuccess: boolean) => Swal({
+  text: message,
+  icon: isSuccess ? "success" : "error",
+  closeOnClickOutside: true,
+  timer: 1500
+});
 
 export const getUserLoginFromToken = async (token: string) => {
   const response = await axios.get(`http://localhost:5000/github/user-data?token=${token}`)
@@ -7,37 +15,54 @@ export const getUserLoginFromToken = async (token: string) => {
 }
 
 export const getTalks = async (token?: string) => {
-  const localToken = localStorage.getItem('token');
-  let user;
+  try {
+    const localToken = localStorage.getItem('token');
+    let user;
 
-  if (localToken) {
-    user = await getUserLoginFromToken(localToken);
+    if (localToken) {
+      user = await getUserLoginFromToken(localToken);
+    }
+
+    const url = user ? `http://localhost:5000/talk?user=${user}` : `http://localhost:5000/talk`;
+    const response = await axios.get(url)
+    return response.data.talks;
+  } catch (error) {
+    notify("Couldn't load talks", false)
+    return []
   }
-
-  const url = user ? `http://localhost:5000/talk?user=${user}` : `http://localhost:5000/talk`;
-  const response = await axios.get(url)
-  return response.data.talks;
 }
 
 
 export const createTalk = async (talk: TalkCardInfo) => {
-  const localToken = localStorage.getItem('token');
-  const { title, description } = talk;
-  const response = await axios.post("http://localhost:5000/talk", { title, description }, {
-    headers: {
-      'Authorization': `${localToken}`
-    }
-  })
-  return response.data.talks;
+  try {
+    const localToken = localStorage.getItem('token');
+    const { title, description } = talk;
+    const response = await axios.post("http://localhost:5000/talk", { title, description }, {
+      headers: {
+        'Authorization': `${localToken}`
+      }
+    })
+
+    notify("Talk Created", true)
+    return response.data.talks;
+  } catch (error) {
+    notify("Try again later :(", false)
+  }
 }
 
 export const voteTalk = async (input: { talkId: String, operation: String }) => {
-  const localToken = localStorage.getItem('token');
-  const response = await axios.put("http://localhost:5000/talk/vote", input, {
-    headers: {
-      'Authorization': `${localToken}`
-    }
-  })
-  return response.data;
+  try {
+    const localToken = localStorage.getItem('token');
+    const response = await axios.put("http://localhost:5000/talk/vote", input, {
+      headers: {
+        'Authorization': `${localToken}`
+      }
+    })
+
+    notify("Voted!", true)
+    return response.data;
+  } catch (error) {
+    notify("Try again later :(", false)
+  }
 }
 
