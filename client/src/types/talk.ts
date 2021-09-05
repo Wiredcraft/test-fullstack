108dba63ea0bd207148a02b3f3be8b1354baf24d
Nicolas Vinicius Teixeira de Lima <teixlima@gmail.com>
2021-09-05 02:33:04 -0300

    feat: adding createdAt optional

--- client/src/types/talk.ts
+++ client/src/types/talk.ts
@@ -1,9 +1,10 @@
 export type TalkCardInfo = {
   _id?: String;
-	title: String;
-	description: String;
-	votes?: String[];
-	author?: String;
+  title: String;
+  description: String;
+  votes?: String[];
+  author?: String;
   voteCount?: Number;
   votedByUser?: Boolean;
+  createdAt?: String;
 }
