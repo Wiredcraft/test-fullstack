80023be14247d4508d4c461584f8e042423de7a3
Nicolas Vinicius Teixeira de Lima <teixlima@gmail.com>
2021-09-04 21:49:03 -0300

    feat: adding form logic to header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -4,39 +4,34 @@ import { useAuth } from "../../contexts/AuthContext";
 import ToggleShowButton from "../toggle-show-button/ToggleShowButton";
 import { REACT_APP_GIT_URL } from "../../../env";
 import CreateTalkForm from "../create-talk-form/CreateTalkForm";
+import { useTalks } from "../../contexts/TalksContext";
 
 function Header() {
-	const { isAuthenticated } = useAuth()
-	const [showCreateTalkForm, setTalkState] = React.useState(false);
-	const GIT_URL = REACT_APP_GIT_URL;
+  const { isAuthenticated } = useAuth()
+  const [showCreateTalkForm, setTalkState] = React.useState(false)
+  const { talks, reloadTalks } = useTalks()
 
-	return (
-		<div className="c-header">
-			<h1>Talkraft</h1>
-			<ToggleShowButton onClick={() => setTalkState(true)} show={isAuthenticated && !showCreateTalkForm} text="Create Talk" />
-			<ToggleShowButton
-				show={!isAuthenticated}
-				text="Sign in with GitHub"
-				href={GIT_URL} />
-			{showCreateTalkForm ? <CreateTalkForm closeForm={() => setTalkState(false)}/> : null}
-		</div>
-	)
+  const getCreateTalkForm = () => showCreateTalkForm ?
+    <CreateTalkForm closeForm={() => setTalkState(false)} /> :
+    null
+
+  return (
+    <div className="c-header">
+      <h1>Talkraft</h1>
+      
+      <ToggleShowButton
+        onClick={() => setTalkState(true)}
+        show={isAuthenticated && !showCreateTalkForm}
+        text="Create Talk" />
+
+      <ToggleShowButton
+        show={!isAuthenticated}
+        text="Sign in with GitHub"
+        href={REACT_APP_GIT_URL} />
+
+      {getCreateTalkForm()}
+    </div>
+  )
 }
-
-//			{
-//				if(isAuthenticated){
-//					return <button >Open Create talk form by Changing state</button>
-//				}
-//			}
-//
-//			{
-//				if(showCreateTalkForm) {
-//					return <LoginForm />
-//				}
-//				if(showCreateTalkForm) {
-//					return <CreateTalkForm />
-//				}
-//			}
-//
 
 export default Header;
