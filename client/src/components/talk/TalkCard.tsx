6a424f02e70178138d2cb4ae62dfbff50715fbbb
Nicolas Vinicius Teixeira de Lima <teixlima@gmail.com>
2021-09-05 02:33:25 -0300

    feat: changing card style

--- client/src/components/talk/TalkCard.tsx
+++ client/src/components/talk/TalkCard.tsx
@@ -16,32 +16,38 @@ function TalkCard(props: InputProps) {
     description,
     title,
     voteCount,
-    votedByUser
+    votedByUser,
+    createdAt
   }, isAuthenticated } = props
 
   const { reloadTalks } = useTalks()
 
   const operation = votedByUser ? "REMOVE" : "ADD"
-  const buttonClass = votedByUser ? "talk-card__button--voted" : "talk-card__button--not-voted"
-  const buttonText = votedByUser ? "unvote" : "vote"
+  const buttonClass = votedByUser ? "talk-card-container__button--voted" : "talk-card-container__button--not-voted"
+  const createdDate = new Date(createdAt.toString())
+  const formattedDate = `${createdDate.getHours()} : ${createdDate.getMinutes()}, ${createdDate.toDateString()}`
 
   const onVote = async () => {
     voteTalk({ talkId: id!, operation })
     const talksFromServer = await getTalks();
-    reloadTalks(talksFromServer)
+    console.log(talksFromServer)
+    talksFromServer.length ? reloadTalks(talksFromServer) : null
   }
 
   return (
-    <div className="talk-card">
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <h3>by: {author}</h3>
+    <div className="talk-card-container">
       {
         isAuthenticated ? <button
           onClick={onVote}
-          className={buttonClass}>{buttonText}: {voteCount}</button>
+          className={buttonClass}>{voteCount}</button>
           : null
       }
+      <div className="talk-card-container--row">
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <h3>by: {author}</h3>
+        <p className="talk-created-date">{formattedDate}</p>
+      </div>
     </div>
   )
 }
