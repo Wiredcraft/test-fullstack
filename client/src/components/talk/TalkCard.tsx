6a86c0788c93abb07064235a8c2a51b31fefd090
Nicolas Vinicius Teixeira de Lima <teixlima@gmail.com>
2021-09-04 21:50:21 -0300

    feat: adding service call to vote operation

--- client/src/components/talk/TalkCard.tsx
+++ client/src/components/talk/TalkCard.tsx
@@ -1,28 +1,41 @@
 import * as React from "react";
+import { voteTalk } from "../../services/talks";
 import { TalkCardInfo } from '../../types/talk';
 import "./talkCard.css";
 
 type InputProps = {
-	isAuthenticated: boolean,
-	cardInfo: TalkCardInfo
+  isAuthenticated: boolean,
+  cardInfo: TalkCardInfo
 }
 
 function TalkCard(props: InputProps) {
-	const { cardInfo: {
-		author,
-		description,
-		title,
-		votes
-	}, isAuthenticated } = props
+  const { cardInfo: {
+    _id: id,
+    author,
+    description,
+    title,
+    votes,
+    voteCount,
+    votedByUser
+  }, isAuthenticated } = props
 
-	return (
-		<div className="talk-card">
-			<h1>{title}</h1>
-			<h3>by: {author}</h3>
-			<p>{description}</p>
-			<button className="talk-card__button--not-voted">upVote: {votes.length}</button>
-		</div>
-	)
+  const operation = votedByUser ? "REMOVE" : "ADD"
+  const buttonClass = votedByUser ? "talk-card__button--voted" : "talk-card__button--not-voted"
+  const buttonText = votedByUser ? "unvote" : "vote"
+
+  return (
+    <div className="talk-card">
+      <h1>{title}</h1>
+      <h3>by: {author}</h3>
+      <p>{description}</p>
+      {
+        isAuthenticated ? <button
+          onClick={() => voteTalk({ talkId: id!, operation })}
+          className={buttonClass}>{buttonText}: {voteCount}</button>
+          : null
+      }
+    </div>
+  )
 }
 
 export default TalkCard;
