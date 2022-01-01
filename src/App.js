f3a5d6c2b97305351f17ba2f2b9fcf8b5ab46963
Taylor Brennan <taylor.j.brennan@gmail.com>
2022-01-01 20:17:18 +0800

    set up react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <h1>Hello, World!</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/preset-react"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    hot: true,
+    open: true
+  }
+};
