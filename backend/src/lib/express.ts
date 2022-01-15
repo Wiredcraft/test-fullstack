5171d11ed571b72bf34dc62e3e3e5e444538a7ba
Loïc PIREZ <loic@pirez.fr>
2022-01-15 10:29:35 +0800

    :sparkles: Inject custom error handling middleware

--- backend/src/lib/express.ts
+++ backend/src/lib/express.ts
@@ -1,5 +1,7 @@
 import {json as bodyParserJson} from 'body-parser';
 import cors from 'cors';
+import handleErrors from '../api/middlewares/handle-errors';
+import notFound from '../api/middlewares/not-found';
 
 /**
  * Initialize `express` server by adding the required middleware and routes.
@@ -7,6 +9,8 @@ import cors from 'cors';
 const initializeExpress: Function = async (): Promise<void> => {
   global.expressApp.use(cors());
   global.expressApp.use(bodyParserJson());
+  global.expressApp.use(handleErrors);
+  global.expressApp.use(notFound);
 };
 
 export default initializeExpress;
