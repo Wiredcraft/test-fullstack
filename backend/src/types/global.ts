0041ca941cd8344bd05272c2123607619f6351a6
Loïc PIREZ <loic@pirez.fr>
2022-01-15 13:34:36 +0800

    :label: Extend `Express.Request` with `currentUser` and `token`

--- backend/src/types/global.ts
+++ backend/src/types/global.ts
@@ -1,10 +1,18 @@
-/* eslint-disable no-unused-vars */ // disabled as we declare a global variable
 /* eslint-disable no-var */ // disabled as we cannot use let in a global scope
 
 import {Application} from 'express';
+import {LeanDocument} from 'mongoose';
 import {Logger} from 'winston';
+import {UserModel} from './models';
 
 declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: LeanDocument<UserModel>;
+      token?: string;
+    }
+  }
+
   var logger: Logger;
   var expressApp: Application;
 }
