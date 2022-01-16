dadf1eaa8a27175b78ac77241325ad8c9da47fed
Loïc PIREZ <loic@pirez.fr>
2022-01-16 11:27:59 +0800

    :recycle: Move `validateParameters` function to the top of source file

--- backend/src/api/middlewares/validate-parameters.ts
+++ backend/src/api/middlewares/validate-parameters.ts
@@ -5,6 +5,26 @@ import generateError from '../generate-error';
 import handleErrors from './handle-errors';
 
 /**
+ * Validate the API parameters of an HTTP request.
+ *
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ * @param {NextFunction} next - Express next middleware function.
+ */
+const validateParameters:
+ RequestHandler = (req: Request, res: Response, next: NextFunction) => {
+   const schema: Joi.ObjectSchema = getObjectSchema(req);
+   const validation: Joi.ValidationResult = schema.validate(req.body);
+
+   validation.error ? handleErrors(
+       generateError(
+           ApiErrorType.ValidationError,
+           validation.error.details.shift()?.message ?? 'Validation error',
+       ), req, res, next,
+   ) : next();
+ };
+
+/**
  * Get an `Joi.ObjectSchema` (validator schema)
  * according the user route to validate
  * the parameters inside an request.
@@ -35,24 +55,5 @@ const getObjectSchema: Function = (req: Request): Joi.ObjectSchema => {
   return Joi.object(constraints);
 };
 
-/**
- * Middleware to validate the API parameters of the HTTP request.
- *
- * @param {Request} req - Express request object.
- * @param {Response} res - Express response object.
- * @param {NextFunction} next - Express next middleware function.
- */
-const validateParameters:
-    RequestHandler = (req: Request, res: Response, next: NextFunction) => {
-      const schema: Joi.ObjectSchema = getObjectSchema(req);
-      const validation = schema.validate(req.body);
-
-      validation.error ? handleErrors(
-          generateError(
-              ApiErrorType.ValidationError,
-              validation.error.details.shift()?.message ?? 'Validation error',
-          ), req, res, next,
-      ) : next();
-    };
 
 export default validateParameters;
