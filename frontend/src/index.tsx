c2f6cd0278307163c890d8fb724904c14b5ba576
Stefan Wawrzyn <swawrzyn@gmail.com>
2022-02-15 21:30:25 +0800

    Init project with NestJS/Postgresql and React
    Project will be using docker/docker-compose for ease of development, with applicable
    docker-compose.yml file at root. A quick docker-compose up will bring up the db,
    api server (nestjs) and react. The frontend is at localhost:3000 while api server at
    localhost:3001.

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+import "core-js/features/array/flat-map";
+import "core-js/features/map";
+import "core-js/features/promise";
+import "core-js/features/set";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ scripts/pgsql/init_db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE wc_fs_dev;
+CREATE DATABASE wc_fs_test;
