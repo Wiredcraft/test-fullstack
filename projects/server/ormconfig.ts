51cc7a9c2a7a950a75b31a1f7eaa2fae26663825
Levi Deng <levi.deng@outlook.com>
2022-03-11 22:43:51 +0800

    chore: initialize the server environment

---
+++ projects/server/ormconfig.ts
@@ -0,0 +1,15 @@
+import { join } from "path";
+import { ConnectionOptions } from "typeorm";
+
+const connectionOptions: ConnectionOptions = {
+  type: "better-sqlite3",
+  database: "db.sqlite",
+  synchronize: false,
+  entities: ["dist/**/*.entity.js"],
+  migrations: ["dist/migration/*.js"],
+  cli: {
+    migrationsDir: "migration",
+  },
+};
+
+export = connectionOptions;
