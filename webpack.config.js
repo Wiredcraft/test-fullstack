2bdcecdfb82b9d14d0bd77677e425b1366716dd1
Bilal Korir <seekingk@live.com>
2022-07-17 22:08:45 +0800

    Configure Webpack with .webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  mode: "development",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    port: 3000,
+    open: false,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+}
